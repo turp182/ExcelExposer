d177f1799d5041b5479d55aeca7fe83ccc741205
turp182 <jason_turpin@yahoo.com>
2015-11-05 07:26:34 -0600

    Initial Addition.
    Initial Addition.

---
+++ formMain.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ExcelExposer
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formMain));
+            this.buttonExpose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxSourceFile = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.checkBoxShowHiddenRows = new System.Windows.Forms.CheckBox();
+            this.checkBoxShowHiddenColumns = new System.Windows.Forms.CheckBox();
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonExpose
+            // 
+            this.buttonExpose.Location = new System.Drawing.Point(173, 38);
+            this.buttonExpose.Name = "buttonExpose";
+            this.buttonExpose.Size = new System.Drawing.Size(293, 41);
+            this.buttonExpose.TabIndex = 0;
+            this.buttonExpose.Text = "Expose!";
+            this.buttonExpose.UseVisualStyleBackColor = true;
+            this.buttonExpose.Click += new System.EventHandler(this.buttonExpose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Source:";
+            // 
+            // textBoxSourceFile
+            // 
+            this.textBoxSourceFile.Location = new System.Drawing.Point(62, 12);
+            this.textBoxSourceFile.Name = "textBoxSourceFile";
+            this.textBoxSourceFile.Size = new System.Drawing.Size(404, 20);
+            this.textBoxSourceFile.TabIndex = 2;
+            this.textBoxSourceFile.Text = "Double click to select file...";
+            this.textBoxSourceFile.DoubleClick += new System.EventHandler(this.textBoxSourceFile_DoubleClick);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "Excel|*.xls;*.xlsx;*.xlsm";
+            // 
+            // checkBoxShowHiddenRows
+            // 
+            this.checkBoxShowHiddenRows.AutoSize = true;
+            this.checkBoxShowHiddenRows.Location = new System.Drawing.Point(15, 38);
+            this.checkBoxShowHiddenRows.Name = "checkBoxShowHiddenRows";
+            this.checkBoxShowHiddenRows.Size = new System.Drawing.Size(139, 17);
+            this.checkBoxShowHiddenRows.TabIndex = 6;
+            this.checkBoxShowHiddenRows.Text = "Show all Hidden Rows?";
+            this.checkBoxShowHiddenRows.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxShowHiddenColumns
+            // 
+            this.checkBoxShowHiddenColumns.AutoSize = true;
+            this.checkBoxShowHiddenColumns.Location = new System.Drawing.Point(15, 62);
+            this.checkBoxShowHiddenColumns.Name = "checkBoxShowHiddenColumns";
+            this.checkBoxShowHiddenColumns.Size = new System.Drawing.Size(152, 17);
+            this.checkBoxShowHiddenColumns.TabIndex = 7;
+            this.checkBoxShowHiddenColumns.Text = "Show all Hidden Columns?";
+            this.checkBoxShowHiddenColumns.UseVisualStyleBackColor = true;
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.Location = new System.Drawing.Point(15, 85);
+            this.textBoxStatus.Multiline = true;
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.ReadOnly = true;
+            this.textBoxStatus.Size = new System.Drawing.Size(451, 102);
+            this.textBoxStatus.TabIndex = 8;
+            this.textBoxStatus.Text = resources.GetString("textBoxStatus.Text");
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 192);
+            this.Controls.Add(this.textBoxStatus);
+            this.Controls.Add(this.checkBoxShowHiddenColumns);
+            this.Controls.Add(this.checkBoxShowHiddenRows);
+            this.Controls.Add(this.textBoxSourceFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonExpose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "formMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Excel Exposer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonExpose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxSourceFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.CheckBox checkBoxShowHiddenRows;
+        private System.Windows.Forms.CheckBox checkBoxShowHiddenColumns;
+        private System.Windows.Forms.TextBox textBoxStatus;
+    }
+}
+
---
+++ formMain.cs
@@ -0,0 +1,254 @@
+﻿//The MIT License (MIT)
+
+//Copyright (c) 20015 Jason Wilbur Turpin
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the Software), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelExposer
+{
+    public partial class formMain : Form
+    {
+        private const string BUTTON_TEXT_DEFAULT = "Expose!";
+        private const string BUTTON_TEXT_EXPOSING = "Exposing...";
+
+        public formMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExpose_Click(object sender, EventArgs e)
+        {
+            buttonExpose.Text = BUTTON_TEXT_EXPOSING;
+            this.Enabled = false;
+
+            Excel.Application excelApp = null;
+
+            Excel.Workbook sourceWorkbook = null;            
+
+            excelApp = new Excel.Application();             
+            excelApp.Visible = false;
+            excelApp.DisplayAlerts = false;
+
+            textBoxStatus.Text = string.Empty;
+
+            UpdateStatus("Opening the Source Spreadsheet.");            
+            
+            sourceWorkbook = excelApp.Workbooks.Open(textBoxSourceFile.Text);
+
+            if (sourceWorkbook.ProtectStructure)
+            {
+                UpdateStatus("Removing Strucural Protection");
+
+                sourceWorkbook = UnprotectWorkbookStructure(sourceWorkbook);
+            }
+
+            int endRow, endColumn;
+
+            int sourceSheetCount = sourceWorkbook.Sheets.Count;
+
+            // overall paste, formulas are set afterwards
+            for (int sourceSheetIndex = 1; sourceSheetIndex <= sourceWorkbook.Sheets.Count; sourceSheetIndex++)
+            {
+                Excel.Worksheet sourceSheet = sourceWorkbook.Sheets[sourceSheetIndex];
+
+                UpdateStatus(string.Format("Removing Protection and Showing Sheet {0}/{1}, '{2}'", sourceSheetIndex, sourceSheetCount, sourceSheet.Name));
+
+                if (sourceSheet.ProtectContents)
+                {
+                    sourceSheet = UnprotectSheet(sourceSheet);
+                }
+
+                if (sourceSheet.Visible == Excel.XlSheetVisibility.xlSheetHidden || sourceSheet.Visible == Excel.XlSheetVisibility.xlSheetVeryHidden)
+                {
+                    sourceSheet.Visible = Excel.XlSheetVisibility.xlSheetVisible;
+                    sourceSheet.Tab.Color = Color.Red;
+                }
+
+                if (sourceSheet == null)
+                {
+                    MessageBox.Show(string.Format("Couldn't determine the password for the sheet '{0}'", sourceSheet.Name));
+                }
+
+                Excel.Range sourceUsedRange = sourceSheet.UsedRange;
+
+                endRow = sourceUsedRange.Row + sourceUsedRange.Rows.Count;
+                endColumn = sourceUsedRange.Column + sourceUsedRange.Columns.Count;
+
+                Marshal.FinalReleaseComObject(sourceUsedRange);
+
+                Excel.Range sourceCells = sourceSheet.Range[sourceSheet.Cells[1, 1], sourceSheet.Cells[endRow, endColumn]];
+
+                // show all rows and columns?
+                if (checkBoxShowHiddenRows.Checked)
+                {
+                    sourceCells.EntireRow.Hidden = false;
+                }
+
+                if (checkBoxShowHiddenColumns.Checked)
+                {
+                    sourceCells.EntireColumn.Hidden = false;
+                }
+
+                Marshal.FinalReleaseComObject(sourceCells);
+                Marshal.FinalReleaseComObject(sourceSheet);
+            }
+
+            excelApp.Visible = true;
+            excelApp.DisplayAlerts = true;
+            Marshal.FinalReleaseComObject(excelApp);
+
+            this.Enabled = true;
+            buttonExpose.Text = BUTTON_TEXT_DEFAULT;
+            UpdateStatus("Finished!");            
+
+            MessageBox.Show("Finished, the Spreadsheet is Exposed!", "Excel Exposer!", MessageBoxButtons.OK);
+        }
+
+        private void textBoxSourceFile_DoubleClick(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                textBoxSourceFile.Text = openFileDialog.FileName;
+            }
+
+        }
+
+        private static string _worksheetPassword = string.Empty;
+
+        private Excel.Worksheet UnprotectSheet(Excel.Worksheet sheet)
+        {
+            if (!string.IsNullOrEmpty(_worksheetPassword))
+            {
+                try
+                {
+                    sheet.Unprotect(_worksheetPassword);
+                }
+                catch
+                {
+
+                }
+
+                if (!sheet.ProtectContents)
+                {                    
+                    return sheet;
+                }
+            }
+
+            // This code is based on a routine found here (public university, University of Wisconsin, Green Bay): https://uknowit.uwgb.edu/page.php?id=28850
+
+            int attemptCount = 0;
+
+            for (int i = 65; i < 67; i++)
+                for (int j = 65; j < 67; j++)
+                    for (int k = 65; k < 67; k++)
+                        for (int l = 65; l < 67; l++)
+                            for (int m = 65; m < 67; m++)
+                                for (int i1 = 65; i1 < 67; i1++)
+                                    for (int i2 = 65; i2 < 67; i2++)
+                                        for (int i3 = 65; i3 < 67; i3++)
+                                            for (int i4 = 65; i4 < 67; i4++)
+                                                for (int i5 = 65; i5 < 67; i5++)
+                                                    for (int i6 = 65; i6 < 67; i6++)
+                                                        for (int n = 32; n < 126; n++)
+                                                        {
+                                                            _worksheetPassword = Convert.ToChar(i).ToString() + Convert.ToChar(j).ToString() + Convert.ToChar(k).ToString() + Convert.ToChar(l).ToString() + Convert.ToChar(m).ToString() +
+                                                                Convert.ToChar(i1).ToString() + Convert.ToChar(i2).ToString() + Convert.ToChar(i3).ToString() + Convert.ToChar(i4).ToString() + Convert.ToChar(i5).ToString() +
+                                                                Convert.ToChar(i6).ToString() + Convert.ToChar(n).ToString();
+
+                                                            attemptCount++;
+
+                                                            try
+                                                            {
+                                                                sheet.Unprotect(_worksheetPassword);
+                                                            }
+                                                            catch { }                                                          
+
+                                                            if (!sheet.ProtectContents)
+                                                            {
+                                                                return sheet;
+                                                            }
+                                                        }
+
+            return null;
+        }
+
+        private Excel.Workbook UnprotectWorkbookStructure(Excel.Workbook workbook)
+        {
+            string workbookStructurePassword = string.Empty;
+
+            int attemptCount = 0;
+
+            // This code is based on a routine found here (public university, University of Wisconsin, Green Bay): https://uknowit.uwgb.edu/page.php?id=28850
+
+            for (int i = 65; i < 67; i++)
+                for (int j = 65; j < 67; j++)
+                    for (int k = 65; k < 67; k++)
+                        for (int l = 65; l < 67; l++)
+                            for (int m = 65; m < 67; m++)
+                                for (int i1 = 65; i1 < 67; i1++)
+                                    for (int i2 = 65; i2 < 67; i2++)
+                                        for (int i3 = 65; i3 < 67; i3++)
+                                            for (int i4 = 65; i4 < 67; i4++)
+                                                for (int i5 = 65; i5 < 67; i5++)
+                                                    for (int i6 = 65; i6 < 67; i6++)
+                                                        for (int n = 32; n < 126; n++)
+                                                        {
+                                                            workbookStructurePassword = Convert.ToChar(i).ToString() + Convert.ToChar(j).ToString() + Convert.ToChar(k).ToString() + Convert.ToChar(l).ToString() + Convert.ToChar(m).ToString() +
+                                                                Convert.ToChar(i1).ToString() + Convert.ToChar(i2).ToString() + Convert.ToChar(i3).ToString() + Convert.ToChar(i4).ToString() + Convert.ToChar(i5).ToString() +
+                                                                Convert.ToChar(i6).ToString() + Convert.ToChar(n).ToString();
+
+                                                            attemptCount++;
+
+                                                            try
+                                                            {
+                                                                workbook.Unprotect(workbookStructurePassword);
+                                                            }
+                                                            catch { }
+                                                            
+
+                                                            if (!workbook.ProtectStructure)
+                                                            {
+                                                                return workbook;
+                                                            }
+                                                        }
+
+            return null;
+        }
+
+        private void UpdateStatus(string status)
+        {
+            if (textBoxStatus.Text != string.Empty)
+            {
+                textBoxStatus.AppendText(Environment.NewLine);
+            }
+            
+            textBoxStatus.AppendText(status);
+            this.Refresh();
+        }
+
+    }
+}
